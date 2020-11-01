0df89de5a0cdcd2bb45a39f49e69e0c8756bd992
Silvino-Cardoso <silvino.itp@gmail.com>
2020-11-01 11:06:30 -0300

    Adicionado primeiro projeto Angular

---
+++ Angular/course-manager/src/app/app.module.ts
@@ -0,0 +1,33 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms'
+import { HttpClientModule } from '@angular/common/http'
+
+import { RouterModule } from '@angular/router'
+
+import { AppComponent } from './app.component';
+
+import { CourseModule } from './courses/course.module';
+import { CoreModule } from './core/core.module';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    CourseModule,
+    CoreModule,
+    RouterModule.forRoot([
+      {
+        path:'' , redirectTo: 'courses', pathMatch:'full'
+      },
+    
+    ])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
